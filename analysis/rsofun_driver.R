8cd76ffea9c7a25b33251566f088f10526d2a127
Koen Hufkens <koen.hufkens@gmail.com>
2023-01-20 19:15:52 +0100

    split function calls

--- analysis/rsofun_driver.R
+++ analysis/rsofun_driver.R
@@ -9,47 +9,5 @@ source("R/driver_download.R")
 # set your user ID
 user <- "2088"
 
-variables <- c(
-  "2m_temperature",
-  "2m_temperature",
-  "2m_temperature",
-  "2m_dewpoint_temperature",
-  "surface_pressure",
-  "total_cloud_cover",
-  "snowfall",
-  "total_precipitation",
-  "clear_sky_direct_solar_radiation_at_surface"
-)
-
-methods <- c(
-  "mean",
-  "min",
-  "max",
-  "mean",
-  "mean",
-  "mean",
-  "sum",
-  "sum",
-  "max"
-)
-
-settings <- data.frame(
-  variables = variables,
-  methods = methods
-)
-
-drivers <- apply(settings, 1, function(x){
-  output <- driver_download(
-    user = user,
-    lon = 20,
-    lat = 50,
-    var = x["variables"],
-    start_date = "2021-01-01",
-    end_date = "2021-03-30",
-    method = x["methods"]
-  )
-
-  return(output)
-})
-
-drivers_agg <- bind_cols(drivers)
+# download formatted data
+format_driver(user, site_info)
